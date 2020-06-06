4c41604773089bf2f72aef52ffd67e6386f1d1de
Zee Mai <zihao_mai@hotmail.com>
2020-06-06 14:27:12 +0100

    Created components for the game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tetris from './components/Tetris';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => {
+  <div className='App'>
+    <Tetris />
+  </div>
+};
 
 export default App;
