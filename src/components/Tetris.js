bba4a1edb07384bc56c26515b2b97a85b20690d6
Zee Mai <zihao_mai@hotmail.com>
2020-06-08 23:32:00 +0100

    Added styling for component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
-import { createStage } from '../gameHelper';
+import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-    return (
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text='Score' />
-                    <Display text='Rows' />               
-                    <Display text='Level' />
-                </div>
-                <StartButton />
-            </aside>
-        </div>
-    )
-}
+  return (
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
 export default Tetris;
