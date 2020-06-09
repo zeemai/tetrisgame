268aeea7df56cb194e483f35c23304a33f837912
Zee Mai <zihao_mai@hotmail.com>
2020-06-09 10:33:43 +0100

    Update collision detection and hook

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
-const Cell = ( { type } ) => (
-    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
-);
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+)
 
 export default Cell;
